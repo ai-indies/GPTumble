40593d7c2f484515daa2964b39659254a11df97e
Pavel Trukhanov <pavel@ds.dfci.harvard.edu>
2024-02-08 00:45:52 -0500

    some renamings everywhere. sorry, couldn't hold myself back

--- pboard.py
+++ pboard.py
@@ -10,56 +10,59 @@ Transition from odd row is same/+1, from even row -1/same
 import numpy as np
 from enum import Enum
 import logging
-
+from collections import namedtuple
+import random
 
 DEBUG = 0
 
+
 logger = logging.Logger(f"Board logger", logging.DEBUG if DEBUG else logging.INFO)
 
-from collections import namedtuple
 
-_side_val_class = namedtuple("side", ["val", "name"])
+Side = namedtuple("Side", ["val", "name"])
 
+LEFT_SIDE = Side(0, "L")
+RIGHT_SIDE = Side(1, "R")
 
-class Side(Enum):
-    LEFT = _side_val_class(0, "l")
-    RIGHT = _side_val_class(1, "r")
+SIDE_VAL_TO_SIDE = {side.val: side for side in [LEFT_SIDE, RIGHT_SIDE]}
+# SIDE_NAME_TO_VAL = {side.name: side.val for side in [LEFT_SIDE, RIGHT_SIDE]}
 
 
 class Board:
     rng = np.random.default_rng()
 
-    def __init__(self, width, height, R_init_chance=False, weights_offbalance=False) -> None:
+    def __init__(self, width, height, R_init_chance=False, offbalance=False) -> None:
         self.width = width
         self.height = height
         self.params = {
-            "L": {
-                "ball_from_l": {
-                    "ball_to_l": self._get_almost_non_rnd_params(0, weights_offbalance),
-                    "to_state_R": self._get_almost_non_rnd_params(1, weights_offbalance),
+            LEFT_SIDE: {
+                "ball_from_L": {
+                    "ball_to_l": self._get_almost_non_rnd_params(0, offbalance),
+                    "to_state_R": self._get_almost_non_rnd_params(1, offbalance),
                 },
-                "ball_from_r": {
-                    "ball_to_l": self._get_almost_non_rnd_params(0, weights_offbalance),
-                    "to_state_R": self._get_almost_non_rnd_params(1, weights_offbalance),
+                "ball_from_R": {
+                    "ball_to_l": self._get_almost_non_rnd_params(0, offbalance),
+                    "to_state_R": self._get_almost_non_rnd_params(1, offbalance),
                 },
             },
-            "R": {
-                "ball_from_l": {
-                    "ball_to_l": self._get_almost_non_rnd_params(1, weights_offbalance),
-                    "to_state_R": self._get_almost_non_rnd_params(0, weights_offbalance),
+            RIGHT_SIDE: {
+                "ball_from_L": {
+                    "ball_to_l": self._get_almost_non_rnd_params(1, offbalance),
+                    "to_state_R": self._get_almost_non_rnd_params(0, offbalance),
                 },
-                "ball_from_r": {
-                    "ball_to_l": self._get_almost_non_rnd_params(1, weights_offbalance),
-                    "to_state_R": self._get_almost_non_rnd_params(0, weights_offbalance),
+                "ball_from_R": {
+                    "ball_to_l": self._get_almost_non_rnd_params(1, offbalance),
+                    "to_state_R": self._get_almost_non_rnd_params(0, offbalance),
                 },
             },
         }
 
-        self._state_to_side = {0: "L", 1: "R"}
+        self.states = np.random.choice(
+            [LEFT_SIDE.val, RIGHT_SIDE.val], size=(self.width, self.height), p=[1 - R_init_chance, R_init_chance]
+        )
 
-        self._side_to_state = {v: k for k, v in self._state_to_side.items()}
-
-        self.states = np.random.choice([0, 1], size=(self.width, self.height), p=[1 - R_init_chance, R_init_chance])
+        # converting to normal list so we can store the RIGH/LEFT_SIDE tuples directly
+        self.states = [[SIDE_VAL_TO_SIDE[pos] for pos in row] for row in self.states]
 
     def _as_string(self, ball_pos=None) -> str:
         board_as_slash = self._one_char_board
@@ -74,12 +77,12 @@ class Board:
         ).replace(" * ", "*")
 
     @staticmethod
-    def _state_to_char(state):
-        return "\\" if state == 0 else "/"
+    def _state_to_slash(state):
+        return "\\" if state.name == "L" else "/"
 
     @property
     def _one_char_board(self):
-        return [[self._state_to_char(pos) for pos in row] for row in self.states]
+        return [[self._state_to_slash(pos) for pos in row] for row in self.states]
 
     def _render(self, ball_pos=None):
         return self._as_string(ball_pos)
@@ -99,7 +102,7 @@ class Board:
 
     def roll_from_w(self, w) -> int:
         in_row_pos = w
-        last_move = Side.RIGHT
+        last_move = RIGHT_SIDE
         if DEBUG:
             print("Input:")
             print(" " + (" |" * w + "*" + "| " * (self.width - w - 1)))  # ball over the board
@@ -109,9 +112,9 @@ class Board:
             move = self._roll_w_h(in_row_pos, row, last_move)
 
             if self.is_wide(row):  # wide row
-                in_row_pos += move.value[0] - 1
+                in_row_pos += move.val - 1
             else:
-                in_row_pos += move.value[0]
+                in_row_pos += move.val
 
             if DEBUG:
                 print(self._render((row, in_row_pos)), "\n")
@@ -121,15 +124,15 @@ class Board:
     def _roll_w_h(self, w, h, last_shift: Side) -> Side:
         if self.is_wide(h):
             if w == 0:
-                return Side.RIGHT
+                return RIGHT_SIDE
             if w == self.width - 1:
-                return Side.LEFT
+                return LEFT_SIDE
 
         old_state = state = self.states[h][w]
 
-        param_state = self.params.get(self._state_to_side[state])
+        param_state = self.params[state]
 
-        new_param_pos = param_state.get("ball_from_" + last_shift.value[1])
+        new_param_pos = param_state.get("ball_from_" + last_shift.name)
 
         to_l_prob = new_param_pos.get("ball_to_l")[h][w]
         to_R_prob = new_param_pos.get("to_state_R")[h][w]
@@ -137,16 +140,16 @@ class Board:
         side_roll = self.rng.random()
         state_roll = self.rng.random()
 
-        new_state = state = "L" if state_roll > to_R_prob else "R"
+        new_state = state = LEFT_SIDE if state_roll > to_R_prob else RIGHT_SIDE
 
         # print(f"- new {h}:{w} state {'<' if state == 'L' else '>'}")
 
-        self.states[h][w] = self._side_to_state[new_state]
+        self.states[h][w] = new_state
 
-        move = Side.RIGHT if side_roll > to_l_prob else Side.LEFT
+        move = RIGHT_SIDE if side_roll > to_l_prob else LEFT_SIDE
 
         if DEBUG:
-            print(f"{h}:{w} {self._state_to_char(old_state)} => {self._state_to_char(new_state)}, move: {move.name}")
+            print(f"{h}:{w} {self._state_to_slash(old_state)} => {self._state_to_slash(new_state)}, move: {move.name}")
 
         return move
 
@@ -167,20 +170,28 @@ class Board:
     def nd_roll_from_pos(self, pos):  # returns end bin distribution and state update distribution
         # starting with prob = [0, 0, ..., 1 (pos), 0, ..., 0]
         in_distr = self._row_n_zero_distr(0)
-        in_distr[Side.RIGHT.val, pos] = 1.0
+        in_distr[RIGHT_SIDE, pos] = 1.0
 
         for row_n in range(self.height):
             next_r_prob = self._nd_roll_one_row(in_distr, row_n)
 
             if self.is_wide(row_n):  # wide row
-                in_row_pos += move.value[0] - 1
+                in_row_pos += move.val - 1
             else:
-                in_row_pos += move.value[0]
+                in_row_pos += move.val
 
             if DEBUG:
                 print(self._render((row, in_row_pos)), "\n")
 
         return in_row_pos
+
+    def _get_row_probs(self, row_n):
+        """
+        returns:
+        transition_probs: float array of size row_w * 2 (left / right)
+        state_change_probs: float array of size row_w * 2 (left / right)
+        """
+        raise NotImplemented()
 
     def _nd_roll_one_row(self, in_distr, row_n):
         """
@@ -192,7 +203,7 @@ class Board:
         out_dist = self._row_n_zero_distr(row_n)
         out_state_distr = self._row_n_zero_distr(row_n)
 
-        for sided_in_distr, transition_probs, state_change_probs in zip(in_distr, self._get_row_probs(row_n)):
+        for sided_in_distr, transition_probs, state_change_probs in zip(in_distr, *self._get_row_probs(row_n)):
             sided_out_dist = sided_in_distr * transition_probs  # needs board borders adjustment or does it not?
             out_dist += sided_out_dist
 
@@ -208,10 +219,10 @@ if __name__ == "__main__":
     stats = Counter()
     trans_stats = Counter()
 
-    b = Board(30, 30, R_init_chance=0.1, weights_offbalance=0.9)
+    b = Board(30, 30, R_init_chance=0.1, offbalance=0.9)
     ball_pos = 0  # b.width // 2
 
-    for i in range(1):
+    for i in range(1000):
         prev_pos = ball_pos
         ball_pos = b.roll_from_w(ball_pos)
 
